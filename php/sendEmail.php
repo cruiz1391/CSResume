14665ed6c5c5fc1fc6d5be0960483fcfb8b33c32
Carlos Ruiz <cruiz1391@gmail.com>
2016-02-11 23:56:02 -0500

    text/html changed to sendemail

--- php/sendEmail.php
+++ php/sendEmail.php
@@ -40,7 +40,7 @@ if($_POST) {
 	$headers .= "Reply-To: ". $email . "\r\n";
 	$headers .= "Return-Path: ". $from . "\r\n";
  	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
 
    if (!$error) {
