9f6394ac7fb9772f3170a5bd7dc78d42312049e2
Carlos Ruiz <cruiz1391@gmail.com>
2016-02-12 00:09:25 -0500

    changes to headers

--- php/sendEmail.php
+++ php/sendEmail.php
@@ -37,10 +37,12 @@ if($_POST) {
 
    // Email Headers
 	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $email . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
 	$headers .= "Return-Path: ". $from . "\r\n";
  	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+	$headers .= "X-Priority: 3\r\n";
+	$headers .= "X-Mailer: PHP". phpversion() ."\r\n";
 
 
    if (!$error) {
